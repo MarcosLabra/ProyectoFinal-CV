99e92a23b397c671f4a02378a6a8ec5f232cf0d3
MarcosLabra <mlabra.dev@gmail.com>
2023-02-04 19:00:51 -0300

    hago los media queries para que sea full responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//mobile menu
 const mobileMenu = document.getElementById("mobileMenu")
 const mobileMenuButton = document.getElementById("mobileMenuButton")
 
@@ -11,6 +12,13 @@ mobileMenu.addEventListener('click', () => {
   }
 })
 
+document.getElementById('brand').addEventListener('click', () => {
+  if (mobileMenu.classList.contains('show')) {
+    mobileMenu.classList.toggle('show')
+  }
+})
+
+//Mail footer
 document.getElementById('mailIcon').addEventListener('click', () => {
   document.getElementById('mail').classList.toggle('show')
 })
